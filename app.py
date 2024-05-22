19454de7696c4b61d6a5996114be63a41f8c1212
SebastianStegmann <92033571+SebastianStegmann@users.noreply.github.com>
2024-05-22 16:11:35 +0200

    x

--- app.py
+++ app.py
@@ -20,13 +20,60 @@ def _():
 
 @get("/signup")
 def _():
+    con = sql.connect("users.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM users")
+
+    rows = cursor.fetchall()
     
-    return template("signup.html")
+    users = {}
+    for row in rows:
+        users =+ row
+
+    
+    return template("signup.html", users = users)
 
 
 @post("/signup")
 def _():
-    return {"x": 1}
+    con = sql.connect("users.db")
+    cursor = con.cursor()
+    # cursor.execute("DROP TABLE IF EXISTS criminals")
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        email TEXT,
+                        password TEXT,
+                        active BOOLEAN,
+                        key TEXT,
+                        token TEXT,
+                        created_at INTEGER DEFAULT (strftime('%s', 'now'))
+                        )''')
+
+    cursor = con.cursor()
+
+    email = request.forms.get("email", "")
+    password = request.forms.get("password", "")
+    
+    #TODO hash password
+
+    key = 12345
+    active = False 
+
+    #generate token 
+    token = 1
+
+
+    cursor.execute("INSERT INTO users (email, password, active, key, token) VALUES (?, ?, ?, ?, ?)",
+                   (email, password, active, key, token))
+    
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+
+    # Print each row
+    con.commit()
+    con.close()
+    return json.dumps(rows)
 
 try:
   import production
