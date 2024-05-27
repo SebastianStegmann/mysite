3798d6d84c0ff3745efdfcaf34c3dc54dd755cd2
SebastianStegmann <92033571+SebastianStegmann@users.noreply.github.com>
2024-05-27 17:13:46 +0200

    xxx

--- routes/truncate.py
+++ routes/truncate.py
@@ -1,21 +1,23 @@
-from bottle import post, response, request
 import sqlite3 as sql
+from bottle import post, request, response, HTTPResponse
 
 @post('/truncate')
-def _():
-  try:
-    con = sql.connect("users.db")
-    cursor = con.cursor()
-    data = request.json
-    code = data.get("code","")
+def truncate_database():
+    try:
+        con = sql.connect("users.db")
+        cursor = con.cursor()
+        data = request.json
+        code = data.get("code", "")
 
-    response.content_type = 'application/json'
-    if code != "1911771620":
-      response.type = 400
-      return {"Error": "Wrong code sent"}
-    cursor.execute("DELETE FROM users") 
-    cursor.close()
-    response.type = 200
-    return {"Info": "Database was truncated"}
-  except Exception as Ex:
-    return {"Error": str(Ex)}
+        response.content_type = 'application/json'
+        if code != "1911771620":
+            raise HTTPResponse(status=400, body={"Error": "Wrong code sent"})
+
+        cursor.execute("DELETE FROM users")
+        con.commit()
+        cursor.close()
+        con.close()
+
+        return {"Info": "Database was truncated"}
+    except Exception as Ex:
+        return {"Error": str(Ex)}
